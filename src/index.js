403befa763d16880bb308c0fc8cc0e21f78d6c30
Nomfanelo-S <nomfaneloskhosana2@gmail.com>
2024-10-04 23:59:49 +0200

    Added Api Key and URL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let currenttemperatureElement = document.querySelector(
+    `#current-weather-value`
+  );
+}
+
+function searchCity(city) {
+  let apiKey = `083bfotb109a4cdefa3925a78a10a2f3`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchEngine(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let currentCityElement = document.querySelector("#current-city");
   currentCityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
