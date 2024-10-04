e7579c41214e1e544d31e8ad4d10f670b4089346
Nomfanelo-S <nomfaneloskhosana2@gmail.com>
2024-10-05 01:37:02 +0200

    Added humidity and speed
    Added humidity and speed elements to JS

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ function refreshWeather(response) {
     `#current-temperature-value`
   );
   let currentCityElement = document.querySelector(`#current-city`);
-  currentCityElement.innerHTML = response.data.city;
   let temperature = response.data.temperature.current;
-  currentTemperatureElement.innerHTML = Math.round(
-    response.data.temperature.current
-  );
+  let weatherDescription = document.querySelector(`#weather-description`);
+  let humidityElement = document.querySelector(`#humidity`);
+  let windSpeedElement = document.querySelector(`#wind`);
+  let currentDateElement = document.querySelector(`#current-date`);
+  let date = new Date(response.data.time * 1000);
+  console.log(response.data);
+
+  currentDateElement.innerHTML = `${date.getDay()} ${date.getHours() ${date.getMinutes}}`
+  currentCityElement.innerHTML = response.data.city;
+  weatherDescription.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  currentTemperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
